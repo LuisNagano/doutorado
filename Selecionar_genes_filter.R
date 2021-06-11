71d4d28a6de5f292db13d3035187da3857c13b38
Renan Abib Pastore <pastore.au@gmail.com>
2021-06-10 23:23:24 -0300

    Adicionei um comentário

--- Selecionar_genes_filter.R
+++ Selecionar_genes_filter.R
@@ -1,6 +1,9 @@
 library(dplyr)
 
+# Criando um vetor para armazenar os genes
 target<- c("NOTCH1", "NOTCH2", "SKP2")
 
 vsd_TCGA_GTEX_Adrenal_target <- filter(vsd_TCGA_GTEX_Adrenal, Class %in% target)
+
+
 
